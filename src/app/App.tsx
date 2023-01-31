293d3093365f9d1e8fcde11769b5a82b39effeb8
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-01-31 22:13:24 +0300

    настроили абсолютные пути для tsconfig и webpack resolvers. Выстроили начальную структуру к архитектуре FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import {Link, Route, Routes} from 'react-router-dom';
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from 'react';
 import './styles/index.scss'
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -17,8 +17,8 @@ const App = () => {
 
       <Suspense fallback={<div>Загрузка...</div>}>
         <Routes>
-            <Route path={'/'} element={<MainPageAsync />} />
-            <Route path={'/about'} element={<AboutPageAsync />}/>
+            <Route path={'/'} element={<MainPage />} />
+            <Route path={'/about'} element={<AboutPage />}/>
         </Routes>
       </Suspense>
     </div>
