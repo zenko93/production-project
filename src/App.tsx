2112c95ed321af8b237de75990ff46fe7f1c119f
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-01-31 20:47:17 +0300

    добавили темы (светлая и темная), подготовили всё для легкого добавления тем. Глобальный контекст и хук для тем.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import { Suspense } from 'react';
+import {Suspense} from 'react';
+import './styles/index.scss'
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
 
