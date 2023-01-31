6319d35b73b9cf5a08474b3cb36ba8d27f2cb76c
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-01-31 21:08:40 +0300

    добавили helper classNames по имени с подобной библиотекой, добавили гит игнор

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {Suspense} from 'react';
 import './styles/index.scss'
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
