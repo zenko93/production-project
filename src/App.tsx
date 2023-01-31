970839de209bf5bdb023c9d689473dee8524231f
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-01-31 19:04:49 +0300

    добавили react-router-dom, разбили страницы на чанки с помощью lazy loading и suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Counter from "./components/Counter";
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss'
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from 'react';
 
 const App = () => {
   return (
     <div className='app'>
-      sdfsdgfs
-      <Counter  />
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About</Link>
+
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+            <Route path={'/'} element={<MainPageAsync />} />
+            <Route path={'/about'} element={<AboutPageAsync />}/>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
