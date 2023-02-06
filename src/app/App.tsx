5be7715c1bcd3a1fad3b93e3439be86a31f1958b
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-02-06 19:49:07 +0300

    2 - создали navbar, AppLink добавили возможность кастомизации(theme) компонента applink через пропсы

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
-import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 };
