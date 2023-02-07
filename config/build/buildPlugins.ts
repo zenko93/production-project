53d42d4dc1e8be5725175da1e5a5cda6dbc58729
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-02-07 19:38:13 +0300

    6 - подключили hot reload для webpack (react refresh plugin, HotModuleReplacementPlugin)

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { ReactRefreshPlugin } from "@pmmmwh/react-refresh-webpack-plugin"
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -15,6 +16,8 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshPlugin(),
   ]
 }
