641f9e92b7e8d8c927e536d6839e214384a400ae
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-02-20 18:56:33 +0300

    [4.3] - refactored app theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,20 +5,16 @@ import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 
-const App = () => {
-    const { theme } = useTheme();
-
-    return (
-        <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback="">
-                <Navbar />
-                <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
-        </div>
-    );
-};
+const App = () => (
+    <div className={classNames('app', {}, [])}>
+        <Suspense fallback="">
+            <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </Suspense>
+    </div>
+);
 
 export default App;
