d3463d809f7f78fe9b9a50fc6302ee62c809daf4
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-02-20 18:02:38 +0300

    [4.2] - added redux store, firs entity

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,37 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { AppButton } from 'shared/ui/AppButton/AppButton';
+import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue);
+    const { t } = useTranslation();
+
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+
+    return (
+        <div>
+            <h1 data-testid="value-title">{ counterValue }</h1>
+            <AppButton
+                data-testid="increment-btn"
+                onClick={increment}
+            >
+                {t('increment')}
+            </AppButton>
+            <AppButton
+                data-testid="decrement-btn"
+                onClick={decrement}
+            >
+                {t('decrement')}
+            </AppButton>
+        </div>
+    );
+};
