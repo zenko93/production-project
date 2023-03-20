b9354691a1591bb1021764792013b66ce5f01ece
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-03-20 20:06:20 +0300

    [8.4] - сделали подгрузку данных с бэка, переключение между плиткой и списком для статей.

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,6 +1,7 @@
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
 
-export { ArticleType, ArticleView } from './model/types/article';
+export { ArticleType, ArticleView, Article } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 
 export { ArticleList } from './ui/ArticleList/ArticleList';
+export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
