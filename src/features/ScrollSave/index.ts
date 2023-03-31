6e035eac96c77e5ee08e51db36a958502d25776d
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-03-31 14:52:36 +0300

    фикс сборки попытка 2

--- src/features/ScrollSave/index.ts
+++ src/features/ScrollSave/index.ts
@@ -1,4 +1,10 @@
+import { scrollSaveReducer, scrollSaveActions } from './model/slices/scrollSaveSlice';
+
 export { ScrollSaveSchema } from './model/types/scrollSaveSchema';
 
 export { getScrollSaveByPath } from './model/selectors/scrollSave';
-export { scrollSaveReducer, scrollSaveActions } from './model/slices/ScrollSaveSlice';
+
+export {
+    scrollSaveReducer,
+    scrollSaveActions,
+};
