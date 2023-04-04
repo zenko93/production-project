f08faef06e752e216d215cf10752ea96b03d8d7a
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-04-04 19:34:55 +0300

    [10.6] - добавили dropdown из библиотеки headless ui, добавили меню в navbar

---
+++ src/shared/ui/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { AppButton } from 'shared/ui/AppButton/AppButton';
+import { Dropdown } from './Dropdown';
+
+export default {
+    title: 'shared/Dropdown',
+    component: Dropdown,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Dropdown>;
+
+const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {
+    trigger: <AppButton>Open</AppButton>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+        {
+            content: 'third',
+        },
+    ],
+};
