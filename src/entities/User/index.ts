b819979101ea1df227d3e7d4bfcf8095bac314dc
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-04-12 21:21:38 +0300

    [11.5] - добавили роли, защищенные роуты, редирект на страницу "у вас нет доступа"

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
+export { isUserAdmin, isUserManager, getUserRoles } from './model/selectors/roleSelectors';
 export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
 export { userActions, userReducer } from './model/slice/userSlice';
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -6,9 +6,13 @@ import { ProfilePage } from 'pages/ProfilePage';
 import { ArticlesPage } from 'pages/ArticalesPage';
 import { ArticleDetailsPage } from 'pages/ArticleDetailsPage';
 import { ArticleEditPage } from 'pages/ArticleEditPage';
+import { AdminPanelPage } from 'pages/AdminPanelPage';
+import { ForbiddenPage } from 'pages/ForbiddenPage';
+import { UserRole } from 'entities/User/model/types/user';
 
 export type AppRouteProps = RouteProps & {
     authOnly?: boolean;
+    roles?: UserRole[];
 }
 
 export enum AppRoutes {
@@ -19,6 +23,8 @@ export enum AppRoutes {
     ARTICLE_DETAILS = 'article_details',
     ARTICLE_CREATE = 'article_create',
     ARTICLE_EDIT = 'article_edit',
+    ADMIN_PANEL = 'admin_panel',
+    FORBIDDEN_PAGE = 'forbidden_page',
     // last
     NOT_FOUND = 'not_found'
 }
@@ -31,6 +37,8 @@ export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.ARTICLE_DETAILS]: '/articles/', // + id
     [AppRoutes.ARTICLE_CREATE]: '/articles/new',
     [AppRoutes.ARTICLE_EDIT]: '/articles/:id/edit',
+    [AppRoutes.ADMIN_PANEL]: '/admin',
+    [AppRoutes.FORBIDDEN_PAGE]: '/forbidden-page',
     // last
     [AppRoutes.NOT_FOUND]: '*',
 };
@@ -68,6 +76,16 @@ export const routeConfig: Record<AppRoutes, AppRouteProps> = {
         path: `${RoutePath.article_edit}`,
         element: <ArticleEditPage />,
         authOnly: true,
+    },
+    [AppRoutes.ADMIN_PANEL]: {
+        path: `${RoutePath.admin_panel}`,
+        element: <AdminPanelPage />,
+        authOnly: true,
+        roles: [UserRole.ADMIN, UserRole.MANAGER],
+    },
+    [AppRoutes.FORBIDDEN_PAGE]: {
+        path: `${RoutePath.forbidden_page}`,
+        element: <ForbiddenPage />,
     },
     // last
     [AppRoutes.NOT_FOUND]: {
