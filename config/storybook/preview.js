f6d5dde9ac7e3585fb33f0c506378bb777bbb50b
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-04-28 21:31:46 +0300

    [13.12] - добавили аддон в сторибук для изменения тем, отключили возможности стандартного адона

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,7 +3,7 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator';
-import {Theme} from "@/shared/const/theme";
+import { Theme } from '../../src/shared/const/theme';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,6 +12,14 @@ export const parameters = {
             color: /(background|color)$/i,
             date: /Date$/,
         },
+    },
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+            { name: 'dark', class: Theme.DARK, color: '#000000' },
+            { name: 'orange', class: Theme.ORANGE, color: '#ff9b24' },
+        ],
     },
 };
 
