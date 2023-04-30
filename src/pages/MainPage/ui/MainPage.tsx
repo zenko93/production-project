63f10839f6b6cef9d77bcc2657de976a8c8a87c4
Sychshenko Evgeniy <ye.sychshenko@andersenlab.com>
2023-04-30 22:25:57 +0300

    [14.3] - установили lint-staged штука проверяющая только те файлы на линтинг которые были изменены

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,5 @@
 import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-// import { BugButton } from '@/app/providers/ErrorBoundary';
 import { Page } from '@/widgets/Page';
 import { RatingCard } from '@/entities/Rating';
 
